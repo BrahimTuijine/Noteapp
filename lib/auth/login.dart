75ceeb8fbc81072c54130e955286db72b4f45766
Brahim Tuijine <brahimtuijine220@gmail.com>
2021-07-05 18:12:03 +0200

    Login UI done

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-
   @override
   _LoginState createState() => _LoginState();
 }
@@ -10,7 +9,64 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    );
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+            child: Image.asset("images/login.png",height: 150, width: 150,)
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.supervisor_account),
+                      hintText: "username",
+                      border:
+                          OutlineInputBorder(borderSide: BorderSide(width: 1))),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.password),
+                      hintText: "password",
+                      border:
+                          OutlineInputBorder(borderSide: BorderSide(width: 1))),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Text("if you haven't an account "),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "click here",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(primary: Colors.blue),
+          ),
+          textTheme: TextTheme(
+              headline6: TextStyle(
+            fontSize: 17,
+            color: Colors.white,
+          ))),
       home: Login(),
       routes: {
-        "login" :(context) => Login(),
-        "signup" :(context) => Test(),
-        
+        "login": (context) => Login(),
+        "signup": (context) => Test(),
       },
     );
   }
