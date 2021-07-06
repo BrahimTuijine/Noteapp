6acef80c6eef953bc7c818d4075ebe584e3d119b
Brahim Tuijine <brahimtuijine220@gmail.com>
2021-07-07 00:22:08 +0200

    machiya fel home UI

---
+++ lib/home/homepage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List notes = [
+    {
+      "note": "just klem w barra",
+      "image": "facebook.png",
+    },
+    {
+      "note": "just klem w barra",
+      "image": "facebook.png",
+    },
+    {
+      "note": "just klem w barra",
+      "image": "facebook.png",
+    },
+    {
+      "note": "just klem w barra",
+      "image": "facebook.png",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).buttonColor,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: notes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key("$index"),
+              child: ListNotes(notes: notes[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ListNotes extends StatelessWidget {
+  final notes;
+  ListNotes({this.notes});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Image.asset(
+              "images/note.jpg",
+              fit: BoxFit.cover,
+              height: 80,
+              width: 90,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: ListTile(
+              title: Text("${notes['note']}"),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.edit),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
